3fcccae966c61e5ecdbc24468d46e861775a112d
Maciej Mlynowski <szypki@gmail.com>
2023-01-03 14:49:24 +0100

    Add eventListeners to initOrderForm in js

--- src/js/script.js
+++ src/js/script.js
@@ -64,6 +64,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       //console.log('new Product:', thisProduct);
     }
@@ -124,6 +126,32 @@
         thisProduct.element.classList.toggle('active');
         /* toggle active class on thisProduct.element */
       });
+    }
+
+    initOrderForm() {
+      const thisProduct = this;
+      console.log(thisProduct);
+
+      thisProduct.form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener('change', function () {
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder() {
+      const thisPorduct = this;
+      console.log(thisPorduct);
     }
   }
 
